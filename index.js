be100daae439a3b5ee90f461cafd0fb0489bcb9e
Jason Le <jas0nle@yahoo.com>
2019-05-28 20:41:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,11 +22,9 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
   
 }
 
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!";
-  }
-}
